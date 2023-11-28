78c9eaa3c2782fc63d9e9063c322a2d5d29c05c9
Charonum <78572976+Charonum@users.noreply.github.com>
2023-11-27 16:51:10 -0800

    minor readability and cleanliness upgrades

--- Installer.py
+++ Installer.py
@@ -10,20 +10,18 @@ version = input("Type A, B or C: ")
 
 if version == "A":
     url = "https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar"
-    os.system("curl " + url + " server.jar -O server.jar")
+    os.system(f"curl {url} server.jar -O server.jar")
 
-    f2 = open("eula.txt", "w+")
-    f2.write("eula=True")
-    f2.close()
+    with open("eula.txt", "w+") as f2:
+        f2.write("eula=True")
     ram = input("RAM (in GB): ")
 
     for file in os.listdir("."):
         if fnmatch.fnmatch(file, "minecraft_server.*.*.*.jar"):
             os.rename(file, "server.jar")
 
-    f0 = open("amber-cfg.txt", "w+")
-    f0.write("ram: " + ram)
-    f0.close()
+    with open("amber-cfg.txt", "w+") as f0:
+        f0.write("ram: " + ram)
 
     print("Complete! Cleaning folder...")
 
@@ -32,16 +30,14 @@ if version == "A":
             os.remove(file)
 else:
     url = "https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.14.22/0.11.2/server/jar"
-    urllib.request.urlretrieve(url, "server.jar")
+    os.system(f"curl {url} server.jar -O server.jar")
 
-    f2 = open("eula.txt", "w+")
-    f2.write("eula=True")
-    f2.close()
+    with open("eula.txt", "w+") as f2:
+        f2.write("eula=True")
     ram = input("RAM (in GB): ")
 
-    f0 = open("amber-cfg.txt", "w+")
-    f0.write("ram: " + ram)
-    f0.close()
+    with open("amber-cfg.txt", "w+") as f0:
+        f0.write("ram: " + ram)
 
     print("Complete! Cleaning folder...")
 
